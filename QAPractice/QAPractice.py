a21e8207ac83cc8284822161d7049df3c75515d8
alesyadubrovskaya <alexshamkina28@gmail.com>
2025-08-22 23:32:58 +0300

    New repository for learning Selenium

--- QAPractice/QAPractice.py
+++ QAPractice/QAPractice.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-driver.get('https://qa-practice.netlify.app/checkboxes')
+def test_checkbox():
+    driver.get('https://qa-practice.netlify.app/checkboxes')
+    checkbox1 = driver.find_element(By.CSS_SELECTOR, "#checkbox1").click()
+    checkbox1 = driver.find_element(By.CSS_SELECTOR, "#checkbox1").click()
+    time.sleep(3)
+    driver.quit()
